6509a7cce9e62dfa7aaa92a5cdab5de9cf071595
Michael Roizman <mich4xD@gmail.com>
2018-08-11 00:07:44 -0600

    Added code that deals with issue #9. Because there is a default service for `IPersistedGrantStore`, our custom service is not added due to the use of `TryAddSingleton`

--- IdentityServer4-Samples/IdentityServer4-mongo-AspIdentity/QuickstartIdentityServer/Quickstart/Extension/IdentityServerBuilderExtensions.cs
+++ IdentityServer4-Samples/IdentityServer4-mongo-AspIdentity/QuickstartIdentityServer/Quickstart/Extension/IdentityServerBuilderExtensions.cs
@@ -98,7 +98,7 @@ namespace QuickstartIdentityServer.Quickstart.Extension
         /// <returns></returns>
         public static IIdentityServerBuilder AddPersistedGrants(this IIdentityServerBuilder builder)
         {
-            builder.Services.TryAddSingleton<IPersistedGrantStore, CustomPersistedGrantStore>();
+            builder.Services.AddSingleton<IPersistedGrantStore, CustomPersistedGrantStore>();
             return builder;
         }
 
